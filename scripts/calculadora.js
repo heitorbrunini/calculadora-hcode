b064e70307adddf5b85b6e321f0004dfbc3450a9
heitorbrunini <68927093+heitorbrunini@users.noreply.github.com>
2021-11-27 02:39:26 -0300

    Projeto criado

---
+++ scripts/calculadora.js
@@ -0,0 +1,2 @@
+
+let calculadora = new CalcControler();
---
+++ scripts/controller/CalcControler.js
@@ -0,0 +1,26 @@
+class CalcControler{
+
+    constructor(){
+
+        this._displayCalc= "0";
+        this._dataAtual; 
+
+    };
+
+    get displayCalc(){
+        return this._displayCalc;
+    }
+
+    set displayCalc(valor){
+        this._displayCalc = valor;
+    }
+
+    get dataAtual(){
+        return this._dataAtual;
+    }
+
+    set dataAtual(valor){
+        this._dataAtual = valor;
+    }
+
+}
