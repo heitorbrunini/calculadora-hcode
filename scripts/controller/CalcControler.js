032bb104b7dcbef2a05e9e9696dd8a0e936d86ef
heitorbrunini <68927093+heitorbrunini@users.noreply.github.com>
2021-12-08 09:40:52 -0300

    Update CalcControler.js

--- scripts/controller/CalcControler.js
+++ scripts/controller/CalcControler.js
@@ -97,6 +97,7 @@ class CalcControler {
             if (!clear) {
                 let last = this._operation.pop();
                 let result = eval(this._operation.join(""));
+                this.displayCalc = result;
                 this._operation = [result, last];
             } else if (clear) {
                 this.checkFloat();
